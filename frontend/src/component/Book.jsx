4674cb4e48bbd5817259c196c7b94d4ab3059247
Darya Sinitsa <lindela@mail.ru>
2021-01-12 13:02:22 +0300

    book button

--- frontend/src/component/Book.jsx
+++ frontend/src/component/Book.jsx
@@ -1,42 +1,33 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Button} from '@material-ui/core';
 import './Book.css';
-// import {
-//     Card, CardImg, CardText, CardBody,
-//     CardTitle, CardSubtitle, Button
-// } from 'reactstrap';
 import {
-    Card,  Button
+    Card, Button, ButtonGroup
 } from "react-bootstrap";
 
 
 export class Book extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
-
-
     render = () => {
         const authors = this.props.book.bookAuthor.map(author => author.authorLastName).join(", ");
-        console.log(this.props.book.bookAuthor)
+        // console.log(this.props.book.bookAuthor)
         return (<Card key={this.props.book.bookId} className="book-card">
-                {/*{this.props.book.bookTitle}*/}
-                <Card.Img variant="top" src={this.props.book.picture}/>
-                <Card.Body>
-                    {/*<CardTitle>{this.props.book.bookTitle}</CardTitle>*/}
-                    <Card.Text>
-                        <span>Author(s): {authors}</span><br />
-                        <span>Year: {this.props.book.yearOfIssue}</span><br />
-                        <span>Price: {this.props.book.price}$</span>
-                    </Card.Text>
+            {/*{this.props.book.bookTitle}*/}
+            <Card.Img variant="top" src={this.props.book.picture}/>
+            <Card.Body>
+                {/*<CardTitle>{this.props.book.bookTitle}</CardTitle>*/}
+                <Card.Text>
+                    <span>Author(s): {authors}</span><br/>
+                    <span>Year: {this.props.book.yearOfIssue}</span><br/>
+                    <span>Price: {this.props.book.price}$</span>
+                </Card.Text>
+                <ButtonGroup>
+                    <Button variant="primary" className="button">Buy</Button>
+                    <Button variant="primary" className="button">Favorite</Button>
+                </ButtonGroup>
+            </Card.Body>
 
-                    <Button variant="primary" className="button">Купить</Button>
-                    <Button variant="primary" className="button">В избранное</Button>
-                </Card.Body>
-
-            </Card>)
+        </Card>)
 
     };
 
